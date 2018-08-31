fdd6327ddb07f9d5518dc26c38802c6e85ec76a0
Joel Svensson <svenssonjoel@yahoo.se>
2018-08-31 21:23:04 +0200

    added digit recognition adapted from a piece of code found online.

---
+++ Exploration/NumberRecog/digits.py
@@ -0,0 +1,65 @@
+
+
+# Code from:https://github.com/saimadhu-polamuri/DataAspirant_codes/blob/master/handwritten_digits_recognition/scripts/handwriteen_digits_recognition.py
+# 
+# Indentation had to be fixed for it to make sense.
+# Many operations here seem to be deprecated.
+# TODO: Update to use non-deprecated operations
+# TODO: Learn some python
+# TODO: Understand what is going on here
+
+
+# Required Python Packages
+from tensorflow.examples.tutorials.mnist import input_data
+
+# Download the MNIS dataset
+mnist = input_data.read_data_sets("model_data/", one_hot=True)
+
+# import tensorflow to the environment
+import tensorflow as tf
+
+# initializing parameters for the model
+batch = 100
+learning_rate = 0.01
+training_epochs = 10
+
+# creating placeholders
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+# creating variables
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+# initializing the model
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+# Defining Cost Function
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# Determining the accuracy of parameters
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Implementing Gradient Descent Algorithm
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+
+# Initializing the session
+with tf.Session() as sess:
+  sess.run(tf.initialize_all_variables())
+  
+  # Creating batches of data for epochs
+  for epoch in range(training_epochs):
+      batch_count = int(mnist.train.num_examples / batch)
+      for i in range(batch_count):
+          batch_x, batch_y = mnist.train.next_batch(batch)
+
+          # Executing the model
+          sess.run([train_op], feed_dict={x: batch_x, y_: batch_y})
+
+          # Print Accuracy of the model
+          if epoch % 2 == 0:
+              print("Epoch: ",epoch)
+              print("Accuracy: ", accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+              print("Model Execution Complete")
+
---
+++ Exploration/activate_venv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source $HOME/tensorflow_venv/venv/bin/activate
+
+
